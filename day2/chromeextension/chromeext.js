89756bfcb8c0e632192dc921ef1117b16d77ba64
Fathima rasha <rashawin25@gmail.com>
2026-07-13 12:28:56 +0530

    day3/1/chromextenstion all issue fixed and deployed for public

--- day2/chromeextension/chromeext.js
+++ day2/chromeextension/chromeext.js
@@ -1,6 +1,8 @@
 let leadarr=[];
 const inputbtn=document.getElementById("input-btn");
 const inputelt=document.getElementById("input-elt");
+const tabbtn=document.getElementById("tab-btn");
+const dltbtn=document.getElementById("dlt-btn");
 const inputurl=document.getElementById("input-url");
 let ulelt=document.getElementById("ul-elt");
 let leadarrfromlocalstorage=localStorage.getItem("mylead");
@@ -16,6 +18,36 @@ inputbtn.addEventListener("click",function(){
     inputelt.value="";
     inputurl.value="";
 });
+
+tabbtn.addEventListener("click", function () {
+
+    chrome.tabs.query(
+        { active: true, currentWindow: true },
+        function (tabs) {
+
+            console.log(tabs);
+
+            leadarr.push({
+                title: tabs[0].title,
+                url: tabs[0].url
+            });
+
+            localStorage.setItem("mylead", JSON.stringify(leadarr));
+
+            rendercontent();
+        }
+    );
+
+});
+
+dltbtn.addEventListener("dblclick",function(){
+    localStorage.removeItem("mylead");
+
+    leadarr = [];
+
+    rendercontent();
+})
+
 function rendercontent(){
     let list="";
     for(let i=0;i<leadarr.length;i++){
